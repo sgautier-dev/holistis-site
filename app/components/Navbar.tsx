0ef5570cc89a346713fc260d03f9a87feda23a0d
sgautier-dev <sgautier.dev@gmail.com>
2023-04-03 15:04:09 +0400

    design newsletter form updated

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 export default function Navbar() {
 	return (
 		<nav className=" bg-blue p-4 sticky top-0 drop-shadow-xl z-10">
-			<div className="mx-auto flex items-center justify-between flex-col sm:flex-row">
-				<Link href="/" className="hover:opacity-80">
+			<div className="mx-auto flex items-center justify-between gap-1 flex-col sm:flex-row">
+				<Link href="/" className="hover:opacity-80 focus-visible:outline-orange">
 					<Image
-						className="border-2 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full"
+						className="border-2 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full "
 						src="/images/boris-profil-787x787.jpg"
 						width={70}
 						height={70}
@@ -15,7 +15,7 @@ export default function Navbar() {
 						priority={true}
 					/>
 				</Link>
-				<Link href="/" className="hover:opacity-80">
+				<Link href="/" className="hover:opacity-80 focus-visible:outline-orange">
 					<Image
 						src="/images/LogoHolistis-Orange+Violet.gif"
 						width={250}
--- app/components/NewsletterForm.tsx
+++ app/components/NewsletterForm.tsx
@@ -1,11 +1,11 @@
 export default function NewsletterForm() {
 	return (
-		<div className="relative isolate overflow-hidden bg-gray-900 py-16 sm:py-24 lg:py-32">
+		<div className="relative isolate overflow-hidden bg-blue py-16 sm:py-24 lg:py-32">
 			<div className="mx-auto max-w-7xl px-6 lg:px-8">
-				<div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
+				<div className="mx-auto max-w-2xl lg:max-w-none grid place-content-center">
 					<div className="max-w-xl lg:max-w-lg">
 						<h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-							Subscribe to our newsletter.
+						Se connecter à la newsletter.
 						</h2>
 						<p className="mt-4 text-lg leading-8 text-gray-300">
 							Nostrud amet eu ullamco nisi aute in ad minim nostrud adipisicing
@@ -13,7 +13,7 @@ export default function NewsletterForm() {
 						</p>
 						<div className="mt-6 flex max-w-md gap-x-4">
 							<label htmlFor="email-address" className="sr-only">
-								Email address
+								Adresse e-mail
 							</label>
 							<input
 								id="email-address"
@@ -21,14 +21,14 @@ export default function NewsletterForm() {
 								type="email"
 								autoComplete="email"
 								required
-								className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-								placeholder="Enter your email"
+								className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-orange sm:text-sm sm:leading-6"
+								placeholder="Saisissez votre e-mail"
 							/>
 							<button
 								type="submit"
-								className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+								className="flex-none rounded-md bg-orange px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange"
 							>
-								Subscribe
+								S&apos;abonner
 							</button>
 						</div>
 					</div>
@@ -39,7 +39,7 @@ export default function NewsletterForm() {
 				aria-hidden="true"
 			>
 				<div
-					className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+					className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-blue to-orange opacity-30"
 					style={{
 						clipPath:
 							"polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
