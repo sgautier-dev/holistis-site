561c1ea54b0d26fe15afc308d10f3bc12100fe37
sgautier-dev <sgautier.dev@gmail.com>
2023-05-11 17:54:21 +0400

    added home cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image'
 import NewsletterForm from './components/NewsletterForm'
+import HomeCards from './components/HomeCards'
 
 export default function Home() {
   return (
     <main className='px-6 lg:px-8 py-20 sm:py-24 mx-auto min-h-screen'>
       <h1 className='mb-12 text-5xl text-center text-orange font-bariol'>En cours de développement 🙏🏼</h1>
+      <HomeCards/>
       <NewsletterForm />
     </main>
   )
