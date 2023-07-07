70677a0684e1df7590f9ce42a5269c5ef2aa669f
sgautier-dev <sgautier.dev@gmail.com>
2023-07-07 17:28:14 +0400

    samesame and ressources added to menu

--- lib/navigation.ts
+++ lib/navigation.ts
@@ -2,6 +2,8 @@ export const navigation = {
     menu: [
 		{ name: "Accueil", href: "/", current: true },
 		{ name: "Overview", href: "/overview", current: false },
+		{ name: "Same Same", href: "/samesame", current: false },
+		{ name: "Ressources", href: "/ressources", current: false },
 		{ name: "Contact", href: "/contact", current: false },
 	],
 	legal: [
