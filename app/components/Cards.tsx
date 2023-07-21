0630bce947c29b6b7669e5d8f53cbdbf29338a20
sgautier-dev <sgautier.dev@gmail.com>
2023-07-21 16:51:27 +0400

    removed titles and dates of same same cards

--- app/components/Cards.tsx
+++ app/components/Cards.tsx
@@ -26,11 +26,11 @@ export default function Cards({ cards }: SameSameProps) {
 	return (
 		<div className="mx-auto mt-16 max-w-7xl px-6 lg:px-8">
 			<ItemFilter
-					uniqueItems={uniqueCategories}
-					selectedItem={selectedCategory}
-					setSelectedItem={setSelectedCategory}
-					allLabel={ALL_CATEGORIES_LABEL}
-				/>
+				uniqueItems={uniqueCategories}
+				selectedItem={selectedCategory}
+				setSelectedItem={setSelectedCategory}
+				allLabel={ALL_CATEGORIES_LABEL}
+			/>
 			<div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none sm:grid-cols-2 xl:grid-cols-3">
 				{filteredByCategories.map((card) => (
 					<article
@@ -38,7 +38,7 @@ export default function Cards({ cards }: SameSameProps) {
 						className="flex flex-col items-start justify-between"
 					>
 						<div className="group relative">
-							<div className="relative w-full group-hover:scale-105 transition-transform duration-500 ease-in-out">
+							<div className="relative w-full group-hover:scale-110 transition-transform duration-500 ease-in-out">
 								<Image
 									src={urlForImage(card.image).url()}
 									alt={card.image.alt}
@@ -49,16 +49,16 @@ export default function Cards({ cards }: SameSameProps) {
 								<div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-orange/60" />
 							</div>
 							<div className="max-w-xl">
-								<div className="mt-8 flex items-center gap-x-4 text-xs text-white">
+								{/* <div className="mt-8 flex items-center gap-x-4 text-xs text-white">
 									<time dateTime={card.publishedAt} className="mr-8">
 										{formatDate(card.publishedAt)}
 									</time>
-								</div>
+								</div> */}
 
-								<h3 className="mt-3 text-lg font-semibold leading-6 text-orange group-hover:scale-105 transition-transform duration-500 ease-in-out">
+								{/* <h3 className="mt-3 text-lg font-semibold leading-6 text-orange group-hover:scale-105 transition-transform duration-500 ease-in-out">
 									{card.title}
-								</h3>
-								<div className="mt-5 text-sm leading-6 text-white line-clamp-2 group-hover:line-clamp-none">
+								</h3> */}
+								<div className="mt-5 text-sm leading-6 text-white">
 									<PortableText value={card.text} />
 								</div>
 							</div>
