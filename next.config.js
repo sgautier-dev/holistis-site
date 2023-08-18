138ace01d43550130dc5466dd5bda5254d700612
sgautier-dev <sgautier.dev@gmail.com>
2023-08-18 18:36:09 +0400

    updated all dependencies and added webpack fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,20 @@
 const nextConfig = {
   images: {
     domains: ['cdn.sanity.io'],
-  }
+  },
+  webpack: (config) => {
+    let modularizeImports = null;
+    config.module.rules.some((rule) =>
+      rule.oneOf?.some((oneOf) => {
+        modularizeImports =
+          oneOf?.use?.options?.nextConfig?.modularizeImports;
+        return modularizeImports;
+      }),
+    );
+    if (modularizeImports?.["@headlessui/react"])
+      delete modularizeImports["@headlessui/react"];
+    return config;
+  },
 }
 
 module.exports = nextConfig
