d2f1cd44304534a8d2b7cb744ae7bc981bcbbd91
sgautier-dev <sgautier.dev@gmail.com>
2023-08-21 16:05:06 +0400

    jest testing installed and configured

--- next.config.js
+++ next.config.js
@@ -3,19 +3,19 @@ const nextConfig = {
   images: {
     domains: ['cdn.sanity.io'],
   },
-  webpack: (config) => {
-    let modularizeImports = null;
-    config.module.rules.some((rule) =>
-      rule.oneOf?.some((oneOf) => {
-        modularizeImports =
-          oneOf?.use?.options?.nextConfig?.modularizeImports;
-        return modularizeImports;
-      }),
-    );
-    if (modularizeImports?.["@headlessui/react"])
-      delete modularizeImports["@headlessui/react"];
-    return config;
-  },
+  // webpack: (config) => {
+  //   let modularizeImports = null;
+  //   config.module.rules.some((rule) =>
+  //     rule.oneOf?.some((oneOf) => {
+  //       modularizeImports =
+  //         oneOf?.use?.options?.nextConfig?.modularizeImports;
+  //       return modularizeImports;
+  //     }),
+  //   );
+  //   if (modularizeImports?.["@headlessui/react"])
+  //     delete modularizeImports["@headlessui/react"];
+  //   return config;
+  // },
 }
 
 module.exports = nextConfig
