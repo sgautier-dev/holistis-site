3a39fb0284bc568244d71dec7ae3ff0f170d7582
sgautier-dev <sgautier.dev@gmail.com>
2023-08-22 11:31:12 +0400

    contact route refactored

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,6 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 import logger from "@/lib/logger";
 const nodemailer = require("nodemailer");
+const validator = require("validator");
+
+const requiredEnvVariables = [
+	"SMTP_HOST",
+	"SMTP_PORT",
+	"SMTP_USER",
+	"SMTP_PASS",
+	"RECAPTCHA_SECRET_KEY",
+];
 
 // Create a transporter object with SMTP configuration
 const transporter = nodemailer.createTransport({
@@ -12,6 +21,20 @@ const transporter = nodemailer.createTransport({
 		pass: process.env.SMTP_PASS, // SMTP password
 	},
 });
+
+function sendEmail(
+	firstName: string,
+	lastName: string,
+	email: string,
+	message: string
+) {
+	return transporter.sendMail({
+		from: `${firstName} ${lastName} <${email}>`,
+		to: process.env.SMTP_USER,
+		subject: "Nouveau message du site Holistis",
+		text: message,
+	});
+}
 
 export async function POST(req: NextRequest) {
 	const { firstName, lastName, email, message, token } = await req.json();
@@ -27,8 +50,17 @@ export async function POST(req: NextRequest) {
 			{ message: "Tous les champs du formulaire sont requis !" },
 			{ status: 400 }
 		);
+	if (!validator.isEmail(email)) {
+		return NextResponse.json({ message: "Email invalide" }, { status: 400 });
+	}
 
 	try {
+		for (const variable of requiredEnvVariables) {
+			if (!process.env[variable]) {
+				throw new Error(`missing ${variable} in env variables`);
+			}
+		}
+
 		const verifyRecaptcha = await fetch(verifyRecaptchaUrl);
 		const responseRecaptcha = await verifyRecaptcha.json();
 
@@ -41,25 +73,21 @@ export async function POST(req: NextRequest) {
 			);
 		}
 
-		await transporter.sendMail({
-			from: `${firstName} ${lastName} <${email}>`,
-			to: process.env.SMTP_USER,
-			subject: "Nouveau message du site Holistis",
-			text: message,
-		});
+		await sendEmail(firstName, lastName, email, message);
 
-        return NextResponse.json(
-            {
-                message: "E-mail envoyé avec succès !",
-            },
-            { status: 200 });
-
+		return NextResponse.json(
+			{
+				message: "E-mail envoyé avec succès !",
+			},
+			{ status: 200 }
+		);
 	} catch (error: any) {
 		logger.error(error);
 		const { title, detail, status } = error.response?.body || {};
 		return NextResponse.json(
-			{   title: title,
-                detail: detail,
+			{
+				title: title,
+				detail: detail,
 				message: "Échec de l'envoi de l'e-mail.",
 			},
 			{ status: status || 500 }
