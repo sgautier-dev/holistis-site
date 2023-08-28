73bd57443a2ce2ab98a05e1b99fc6c391358a2cc
sgautier-dev <sgautier.dev@gmail.com>
2023-08-28 17:14:25 +0400

    revalidate route refactored and working

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,8 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
+import logger from "@/lib/logger";
 
 export async function GET(request: NextRequest) {
-	// to use with a webhook: http://localhost:3000/api/revalidate?secret=xxxxx
+	// to use with a webhook: http://localhost:3000/api/revalidate?path=/xxxx&secret=xxxxx
 	const path = request.nextUrl.searchParams.get("path") || "/";
 	const secret = request.nextUrl.searchParams.get("secret");
 
@@ -17,8 +18,12 @@ export async function GET(request: NextRequest) {
 
 	try {
 		revalidatePath(path);
+		if (path === "/overview") {
+			revalidatePath("/ressources");
+		}
 		return NextResponse.json({ revalidated: true, now: Date.now() });
 	} catch (err) {
+		logger.error(err);
 		return NextResponse.json(
 			{
 				message: "Error revalidating",
